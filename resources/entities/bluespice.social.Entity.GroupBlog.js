cf6531b9cbe219168afc8fc6de1e876106009f41
miriam schlindwein <schlindwein@hallowelt.com>
2022-08-26 12:22:28 +0200

    Fix timeline create blog
    ERM28798
    
    [4.2.x]
    
    Change-Id: Ice58086cf9e33965a3995bfe9ad0a8b96c5aa2b9
    (cherry picked from commit 28cbcc93bcfdf09593ac44ad4054f1d4cc22e841)

--- resources/entities/bluespice.social.Entity.GroupBlog.js
+++ resources/entities/bluespice.social.Entity.GroupBlog.js
@@ -1,11 +1,10 @@
 bs.social.EntityGroupBlog = function( $el, type, data ) {
 	bs.social.EntityBlog.call( this, $el, type, data );
-	var me = this;
 };
 OO.initClass( bs.social.EntityGroupBlog );
 OO.inheritClass( bs.social.EntityGroupBlog, bs.social.EntityBlog );
 
-bs.social.EntityBlog.prototype.makeEditor = function() {
+bs.social.EntityGroupBlog.prototype.makeEditor = function() {
 	return new bs.social.EntityEditorGroupBlog(
 		this.getEditorConfig(),
 		this

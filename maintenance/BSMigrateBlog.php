26ce07f7866b5c94d8dcd6d69a6f6326b0997cf0
Hamish Slater <hamishslater@hotmail.com>
2022-09-02 12:23:27 +0200

    Fix deprecated calls to 'User::newFromId'
    Remove multiple calls to 'MediaWikiServices::getInstance'
    
    ERM29676
    
    [4.x]
    
    Change-Id: Ie0cf178ff5478fa931f6f2bd7e6114643c3d1705

--- maintenance/BSMigrateBlog.php
+++ maintenance/BSMigrateBlog.php
@@ -9,6 +9,9 @@ use MediaWiki\MediaWikiServices;
 
 class BSMigrateBlog extends LoggedUpdateMaintenance {
 
+	/** @var MediaWikiServices */
+	private $services = null;
+
 	public function __construct() {
 		parent::__construct();
 		// we hope, that the default blog namespace was used or the current blog
@@ -25,6 +28,7 @@ class BSMigrateBlog extends LoggedUpdateMaintenance {
 			$wgExtraNamespaces[NS_BLOG_TALK] = 'Blog_talk';
 			$GLOBALS['bsgSystemNamespaces'][1503] = 'NS_BLOG_TALK';
 		}
+		$this->services = MediaWikiServices::getInstance();
 	}
 
 	protected $data = [];
@@ -111,7 +115,8 @@ class BSMigrateBlog extends LoggedUpdateMaintenance {
 	 * @return User
 	 */
 	protected function extractUser( Title $title ) {
-		return User::newFromID( $title->getFirstRevision()->getUser() );
+		return $this->services->getUserFactory()
+			->newFromID( $title->getFirstRevision()->getUser() );
 	}
 
 	/**
@@ -119,7 +124,7 @@ class BSMigrateBlog extends LoggedUpdateMaintenance {
 	 * @return EntityFactory
 	 */
 	protected function getFactory() {
-		return MediaWikiServices::getInstance()->getService( 'BSEntityFactory' );
+		return $this->services->getService( 'BSEntityFactory' );
 	}
 
 	/**
@@ -157,7 +162,7 @@ class BSMigrateBlog extends LoggedUpdateMaintenance {
 	 * @return User
 	 */
 	protected function getMaintenanceUser() {
-		return MediaWikiServices::getInstance()->getService( 'BSUtilityFactory' )
+		return $this->services->getService( 'BSUtilityFactory' )
 			->getMaintenanceUser()->getUser();
 	}
 

c62e2a2307aaba42f5d749b2d24df741585458bf
d.savuljesku <d.savuljesku@gmail.com>
2024-07-18 14:32:17 +0200

    Try to relate notification to intended title
    [4.5.x]
    
    ERM38074
    
    Change-Id: I67d2715e62fd369025dbd0f262530193572b1bce

--- src/Event/SocialBlogEvent.php
+++ src/Event/SocialBlogEvent.php
@@ -2,13 +2,41 @@
 
 namespace BlueSpice\Social\Blog\Event;
 
+use BlueSpice\EntityFactory;
 use BlueSpice\Social\Blog\Entity\Blog as BlogEntity;
 use BlueSpice\Social\Event\SocialEvent;
+use MediaWiki\Permissions\GroupPermissionsLookup;
+use MediaWiki\User\UserFactory;
+use MediaWiki\User\UserIdentity;
 use Message;
 use MWStake\MediaWiki\Component\Events\Delivery\IChannel;
 use MWStake\MediaWiki\Component\Events\EventLink;
+use stdClass;
+use TitleFactory;
+use Wikimedia\Rdbms\ILoadBalancer;
 
 class SocialBlogEvent extends SocialEvent {
+
+	/** @var TitleFactory */
+	private $titleFactory;
+
+	/**
+	 * @param ILoadBalancer $lb
+	 * @param UserFactory $userFactory
+	 * @param GroupPermissionsLookup $gpl
+	 * @param EntityFactory $entityFactory
+	 * @param TitleFactory $titleFactory
+	 * @param UserIdentity $agent
+	 * @param stdClass $entityData
+	 * @param string $action
+	 */
+	public function __construct(
+		ILoadBalancer $lb, UserFactory $userFactory, GroupPermissionsLookup $gpl, EntityFactory $entityFactory,
+		TitleFactory $titleFactory, UserIdentity $agent, stdClass $entityData, string $action = self::ACTION_EDIT
+	) {
+		$this->titleFactory = $titleFactory;
+		parent::__construct( $lb, $userFactory, $gpl, $entityFactory, $agent, $entityData, $action );
+	}
 
 	/**
 	 * @return Message
@@ -28,12 +56,33 @@ class SocialBlogEvent extends SocialEvent {
 	}
 
 	/**
+	 * @return \Title|null
+	 */
+	protected function getWatchedTitle() {
+		// For blogs, related title is always Main_page, need to get the actual related page from tags
+		$data = $this->entity->getFullData();
+		if ( isset( $data['tags'] ) && count( $data['tags'] ) ) {
+			$firstRelated = reset( $data['tags'] );
+			$title = $this->titleFactory->newFromText( $firstRelated );
+			if ( $title ) {
+				return $title;
+			}
+		}
+
+		return $this->entity->getRelatedTitle();
+	}
+
+	/**
 	 * @inheritDoc
 	 */
 	public function getLinks( IChannel $forChannel ): array {
+		$relevant = $this->doGetRelevantTitle();
+		if ( !$relevant ) {
+			return [];
+		}
 		return [
 			new EventLink(
-				$this->getTitle()->getFullURL(),
+				$relevant->getFullURL(),
 				Message::newFromKey( 'bs-social-notification-blog-primary-link-label' )
 			)
 		];
